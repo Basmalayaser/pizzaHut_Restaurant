b8ee88bd10c18caef91659c5a411c685894c25da
Basmalayaser <basmalayassser22@gmail.com>
2025-07-22 22:26:15 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/pizzaHut-Restaurant/',
+   base: '/pizzaHut_Restaurant/',
   plugins: [react()],
 })
